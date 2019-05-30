4d6cc54730571ad07ecde7ec1c072cd65d8e2738
enes <enesozturk.d@gmail.com>
2019-05-30 16:54:13 +0300

    add example rn project

---
+++ examples/App.js
@@ -0,0 +1,71 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import { Platform, StyleSheet, Text, View } from 'react-native';
+import SwipeablePanel from 'rn-swipeable-panel';
+
+const instructions = Platform.select({
+	ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+	android: 'Double tap R on your keyboard to reload,\n' + 'Shake or press menu button for dev menu'
+});
+
+type Props = {};
+export default class App extends Component<Props> {
+	constructor(props) {
+		super(props);
+		this.state = {
+			swipeablePanelActive: false
+		};
+	}
+
+	componentDidMount = () => {
+		this.openPanel();
+	};
+
+	openPanel = () => {
+		this.setState({ swipeablePanelActive: true });
+	};
+
+	closePanel = () => {
+		this.setState({ swipeablePanelActive: false });
+		setTimeout(() => {
+			this.openPanel();
+		}, 1000);
+	};
+
+	render() {
+		return (
+			<View style={styles.container}>
+				<Text style={styles.welcome}>Welcome to React Native!</Text>
+				<Text style={styles.instructions}>To get started, edit App.js</Text>
+				<Text style={styles.instructions}>{instructions}</Text>
+				<SwipeablePanel active={this.state.swipeablePanelActive} onClose={() => this.closePanel()} />
+			</View>
+		);
+	}
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		backgroundColor: '#F5FCFF'
+	},
+	welcome: {
+		fontSize: 20,
+		textAlign: 'center',
+		margin: 10
+	},
+	instructions: {
+		textAlign: 'center',
+		color: '#333333',
+		marginBottom: 5
+	}
+});
