ea19cf53a4e10616c6558fd2aaa027483a702b51
enesozturk <enesozturk.d@gmail.com>
2020-04-25 07:21:40 +0300

    Close default only small state

--- examples/App.tsx
+++ examples/App.tsx
@@ -50,7 +50,7 @@ export default class App extends Component<{}, AppState> {
       closeOnTouchOutside: false,
       noBar: false,
       showCloseButton: false,
-      onlySmall: true,
+      onlySmall: false,
     };
   }
 
