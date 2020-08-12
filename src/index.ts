a4f9d77a896d9620405262d6d672e0e0f6550301
enesozturk <enesozturk.d@gmail.com>
2020-08-12 18:13:32 +0300

    Update package configs, remove example app, publish v1.1.12

--- src/index.ts
+++
@@ -1,2 +0,0 @@
-import SwipeablePanel, { LARGE_PANEL_CONTENT_HEIGHT, SMALL_PANEL_CONTENT_HEIGHT } from './components/Panel';
-export { SwipeablePanel as default, LARGE_PANEL_CONTENT_HEIGHT, SMALL_PANEL_CONTENT_HEIGHT };
